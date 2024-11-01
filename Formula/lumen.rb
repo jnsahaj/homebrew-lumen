ad371459f22c570eb49a68f9abfb4c203cc37d61
Sahaj Jain <sahaj.jain@myhq.in>
2024-11-01 18:10:42 +0530

    feat: Create new release 0.5.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
-  desc "AI-powered git commit summaries (No API key required)"
+  desc "Instant AI Git Commit Summaries from CLI (no API key required)"
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/untagged-c1642d74625a23dfc315/lumen.tar.gz"
-  sha256 "b6b823fd13962a6fdd951ffed745f05a697c83b8c9ee4e9c61ec1f9168681f8d"
-  version "0.4.0"
+  url "https://github.com/jnsahaj/lumen/releases/download/v0.5.0/lumen.tar.gz"
+  sha256 "05b4bd8046b7dfa94e309438cbdd239055bc70d82ca2baaadb0dedab8d497f90"
+  version "0.5.0"
 
   def install
     bin.install "lumen"
