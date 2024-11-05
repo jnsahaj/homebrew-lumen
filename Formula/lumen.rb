38be3501e7572ebefc8fdec9f73c611359a812ab
Sahaj Jain <sahaj.jain@myhq.in>
2024-11-05 17:59:34 +0530

    feat: Create new release 1.0.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
-  desc "Instant AI Git Changes Summary from CLI (no API key required)"
+  desc "lumen is a command-line tool that uses AI to generate commit messages, summarise git diffs or past commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/v0.6.0/lumen.tar.gz"
-  sha256 "dab4c2f0f55edbe8b134918515a44191fe5e53d98e3d2be0a402bd270ca687c3"
-  version "0.6.0"
+  url "https://github.com/jnsahaj/lumen/releases/download/v1.0.0/lumen.tar.gz"
+  sha256 "47bb57de6c2f5df32e98e6a47b1da1b159c92616449ed0d16d20a7c80fbdb690"
+  version "1.0.0"
 
   def install
     bin.install "lumen"
