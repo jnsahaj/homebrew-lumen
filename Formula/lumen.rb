a4a80cc697ad07dd83c5d713e37e65241a20d719
Sahaj Jain <sahaj.jain@myhq.in>
2024-11-06 19:11:27 +0530

    Update lumen formula to version 1.1.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
   desc "lumen is a command-line tool that uses AI to generate commit messages, summarise git diffs or past commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/v1.0.0/lumen.tar.gz"
-  sha256 "47bb57de6c2f5df32e98e6a47b1da1b159c92616449ed0d16d20a7c80fbdb690"
-  version "1.0.0"
+  url "https://github.com/jnsahaj/lumen/releases/download/v1.1.0/lumen.tar.gz"
+  sha256 "b9913c3f21eaefe971ff71c52c7458355bc8d36a0db5d7c63da0bb18843a9d3e"
+  version "1.1.0"
 
   def install
     bin.install "lumen"
