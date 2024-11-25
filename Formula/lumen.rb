b795a996a5796a0c5a34bc80cc78de764b1fed26
Sahaj Jain <sahaj.jain@myhq.in>
2024-11-26 01:03:36 +0530

    fix: Update release build sha

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -2,7 +2,7 @@ class Lumen < Formula
   desc "lumen is a command-line tool that uses AI to generate commit messages, summarise git diffs or past commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
   url "https://github.com/jnsahaj/lumen/releases/download/v1.6.0/lumen.tar.gz"
-  sha256 "eef749d1640407dc0b1ce2275665f46298ce232e4d70d75c8c578e6c7b022acc"
+  sha256 "b3cf13ddf872d5c283b6625f8e9796b00e17bdc8ead70bf9c9bb11be5c8a0f95"
   version "1.6.0"
 
   def install
