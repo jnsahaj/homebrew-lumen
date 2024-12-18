6c6a075edb6b52a165be8aa1aa236ea240a6a12c
Sahaj Jain <sahaj.jain@myhq.in>
2024-12-18 17:18:01 +0530

    refactor(Formula/lumen.rb): update url, sha256, and version for lumen v1.7.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
   desc "lumen is a command-line tool that uses AI to generate commit messages, summarise git diffs or past commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/v1.6.0/lumen.tar.gz"
-  sha256 "b3cf13ddf872d5c283b6625f8e9796b00e17bdc8ead70bf9c9bb11be5c8a0f95"
-  version "1.6.0"
+  url "https://github.com/jnsahaj/lumen/releases/download/v1.7.0/lumen.tar.gz"
+  sha256 "890b7e004cb2e6b39018c75da6e15854beb93c1ab5b06f2ac57ebcb2941d725e"
+  version "1.7.0"
 
   def install
     bin.install "lumen"
