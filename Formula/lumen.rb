f7f6c1954c2955a450c70afbce2ffec4b9eed5ee
Sahaj Jain <sahaj.jain@myhq.in>
2024-12-21 00:40:24 +0530

    fix(Formula/lumen): update lumen to v1.7.1 with correct url and sha256 sum.

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
   desc "lumen is a command-line tool that uses AI to generate commit messages, summarise git diffs or past commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/v1.7.0/lumen.tar.gz"
-  sha256 "890b7e004cb2e6b39018c75da6e15854beb93c1ab5b06f2ac57ebcb2941d725e"
-  version "1.7.0"
+  url "https://github.com/jnsahaj/lumen/releases/download/v1.7.1/lumen.tar.gz"
+  sha256 "e3772a0034ce930c749fe8c854ecdaada519c094fa6428eecca53d12a297e166"
+  version "1.7.1"
 
   def install
     bin.install "lumen"
