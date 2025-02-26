9e2a3bd8b88b2b1df874fd5814bb82420785d607
Sahaj Jain <sahaj.jain@myhq.in>
2025-02-26 22:37:15 +0530

    chore: update lumen version to 1.8.0 and corresponding URL and SHA256

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
   desc "lumen is a command-line tool that uses AI to generate commit messages, summarise git diffs or past commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/v1.7.1/lumen.tar.gz"
-  sha256 "e3772a0034ce930c749fe8c854ecdaada519c094fa6428eecca53d12a297e166"
-  version "1.7.1"
+  url "https://github.com/jnsahaj/lumen/releases/download/v1.8.0/lumen.tar.gz"
+  sha256 "86f4cc058b41fa2a53a11c4265a403e31606fc1ade4768b703e96b4aa4c7135f"
+  version "1.8.0"
 
   def install
     bin.install "lumen"
