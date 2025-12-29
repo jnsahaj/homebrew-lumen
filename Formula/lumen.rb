c7a03fe6254ebb1b10cb77396f01e06a3677d862
Sahaj Jain <sahajjain13@gmail.com>
2025-12-30 03:38:20 +0530

    chore: Bump ver to 2.2.1

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,9 +1,9 @@
 class Lumen < Formula
   desc "lumen is a command-line tool that can show pretty diff, generate commit messages with AI, summarise diffs / commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  url "https://github.com/jnsahaj/lumen/releases/download/v2.2.0/lumen.tar.gz"
-  sha256 "ee86f475f6d08b767278bf890a34129ebda0e1f604e254311a30f7208e1b80d6"
-  version "2.2.0"
+  url "https://github.com/jnsahaj/lumen/releases/download/v2.2.1/lumen.tar.gz"
+  sha256 "a10d3d14f0e8cece8eb08f24dbe4e2beda2326eba48166a22b5bb51fa658d87d"
+  version "2.2.1"
 
   def install
     bin.install "lumen"
