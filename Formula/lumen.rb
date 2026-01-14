4042fc5ee1af19d35bf889a2ad18984da1033854
Sahaj Jain <sahajjain13@gmail.com>
2026-01-15 01:26:32 +0530

    chore: Bump ver to 2.16.0

--- Formula/lumen.rb
+++ Formula/lumen.rb
@@ -1,16 +1,16 @@
 class Lumen < Formula
   desc "lumen is a command-line tool that can show pretty diff, generate commit messages with AI, summarise diffs / commits, and more without requiring an API key."
   homepage "https://github.com/jnsahaj/lumen"
-  version "2.15.0"
+  version "2.16.0"
 
   on_intel do
-    url "https://github.com/jnsahaj/lumen/releases/download/v2.15.0/lumen-x86_64-apple-darwin.tar.gz"
-    sha256 "c5819858eb8c2cbb5ea700a571fddbe0f2436ffd1064390ccefb5c3f88a6e068"
+    url "https://github.com/jnsahaj/lumen/releases/download/v2.16.0/lumen-x86_64-apple-darwin.tar.gz"
+    sha256 "e900b6a518c0b9bc62d68a89b1c9b0239f8e510402958b88f7ad94ec4cda2a53"
   end
 
   on_arm do
-    url "https://github.com/jnsahaj/lumen/releases/download/v2.15.0/lumen-aarch64-apple-darwin.tar.gz"
-    sha256 "2e54f1efa5bedfb105664d57d25c5cd15e8f52e8e7adc69e97c2c917d35ce92b"
+    url "https://github.com/jnsahaj/lumen/releases/download/v2.16.0/lumen-aarch64-apple-darwin.tar.gz"
+    sha256 "2a8e38a83f2151de79e10b8326474a0e518787da9694c5df31f2ed8e3b8c9083"
   end
 
   def install
